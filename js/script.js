d7aa426e0d624cc65bc8ec824baf9c5734ac3ce6
tonydevpro <antonio.1358@hotmail.com>
2024-12-27 15:57:55 -0300

    atualizar clean

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ document.querySelector('button').addEventListener('click', ()=>{
     let texto=document.querySelector('input').value;
     let partes = texto.split(',').map(nome => nome.trim());
     let ordenados = partes.sort();
-    console.log(texto)
-    console.log(partes)
-    console.log(ordenados)
+    
     resultado.innerHTML = ordenados.join('<br>');;
 })
